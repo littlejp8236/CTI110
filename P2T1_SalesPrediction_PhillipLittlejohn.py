0b88a0a4103d3226855df157a1b57dd631d10b05
littlejp8236 <43650609+littlejp8236@users.noreply.github.com>
2018-09-27 13:13:48 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_PhillipLittlejohn.py
@@ -0,0 +1,9 @@
+#Get projected total sales
+total_sales = float(input("Enter projected sales: '))
+
+#Calculate profit as 23% of total sales
+profit = total_sales * 0.23
+
+#Display the profit
+print("The profit is $', format(profit, ',,2)
+                          
